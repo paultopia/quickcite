f5a9354e9516ee5e105accd56c9d99c807a7a978
Paul Gowder <paul.gowder@gmail.com>
2015-06-24 21:13:12 -0400

    working script to quick-enter citations and output json

---
+++ qcEnterEd.py
@@ -0,0 +1,9 @@
+# simple scrip to quickly enter academic citations on edtorial in IOS
+# saves a file to dropbox in JSON format with as many citations as you want to add.
+# this can then be put into bibtex, processed directly, etc.
+# rationale: entering stuff into citation managers sucks.  Too much clicking,
+# too many agonizing drop-down menus, too much friction. Death to friction.
+# depends on workflow 
+
+import editor
+import clipboard
